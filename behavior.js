8526c32146916d14d5cbbae90f7d77718402571c
emilianopprogramando <penicheemiliano8272@gmail.com>
2026-05-05 08:55:04 -0600

    Se agrega el javascript a la pagina.

---
+++ behavior.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    var titulo = document.querySelector("h1");
+
+    titulo.addEventListener("click", function () {
+        alert("¡Sigue entrenando!");
+    });
+
+});
